ecd5cb35555092e5b95377bd507269702187cf3f
alaatabbara12344-spec <alaatabbara12344@gmail.com>
2026-09-15 16:19:57 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='tabbara-fish-pos-v26';
+const CACHE_NAME='tabbara-fish-pos-v33';
 const FILES_TO_CACHE=['./','./index.html','./style.css','./script.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES_TO_CACHE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
